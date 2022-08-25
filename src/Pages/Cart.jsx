54b8af8f835af027d67d3e158d3f8bf5dbf2acca
pragati tripathi <mailpragatihere@gmail.com>
2022-08-25 19:32:50 +0530

    cart page

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { Box, Button, Container, Flex, Heading } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  Heading,
+  Divider,
+} from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import "./Cart.css";
 import { ArrowBackIcon } from "@chakra-ui/icons";
@@ -7,7 +14,7 @@ import { ArrowBackIcon } from "@chakra-ui/icons";
 const Cart = () => {
   return (
     <>
-      <Container maxW="90%" padding="1rem">
+      <Container maxW="90%" padding="1rem" className="top">
         {/* ----------------------------Header----------------------------- */}
         <Heading as="h2" size="2xl" padding="0.75rem">
           Cart
@@ -24,33 +31,138 @@ const Cart = () => {
         </Link>
 
         {/* -----------------------------Main Cart------------------------------ */}
-        <Container>
-          <Flex gap="2rem" justifyContent="space-between">
+        <Box className="cart">
+          <Flex direction="column">
             {/* -----------------------Left side for items added--------------------- */}
-            <Box className="items-added">
-              {/* {cartItems.map((items) => { */}
-              <Flex direction="column">
-                {/* -----------------------------image and edit, delete options----------------------------- */}
+            {/* {cartItems.map((item) => { */}
+            {/* ---------------------------------Dummy item1----------------------------------------- */}
+            <Box>
+              <Flex>
                 <Box>
-                  <Flex>
-                    {/* -----------------------------------image-------------------------------- */}
-                    <Box>{/* {items.imageSrc} */}</Box>
-                    <Flex>
-                      <Box>
-                        {/* <Link to="./">  */}
-                        Edit
-                        {/* </Link>  */}
-                      </Box>
-                      <Box> Delete </Box>
-                    </Flex>
+                  <Flex direction="column" p={2}>
+                    <Box>
+                      <img
+                        src="https://media-cdn.tripadvisor.com/media/attractions-splice-spp-360x240/0a/b5/2f/ed.jpg"
+                        alt="image"
+                        width="80%"
+                      />
+                    </Box>
+                    <Box>
+                      <Flex gap={30}>
+                        <Box>Edit</Box>
+                        <Box>Remove</Box>
+                      </Flex>
+                    </Box>
                   </Flex>
                 </Box>
-                {/* ---------------------------------descriptions------------------------------------- */}
-                <Box></Box>
+                <Box>
+                  <Flex direction="column" p={2}>
+                    <Box>
+                      <Heading size="md">
+                        <b>Pune Walking Tour</b>
+                      </Heading>
+                    </Box>
+                    <Box> Pune Walking tour - 9:00</Box>
+                    <Box> Pune Walking tour - 9:00</Box>
+                    <Box>
+                      <Flex gap={10}>
+                        <Box>
+                          2 Adults * <b>₹1743.36</b>
+                        </Box>
+                        <Box>
+                          <b>Total ₹3,486.72</b>
+                        </Box>
+                      </Flex>
+                    </Box>
+                    <br />
+                    <Box>
+                      <Flex>
+                        <Box>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            fill="currentColor"
+                            class="bi bi-phone"
+                            viewBox="0 0 16 16"
+                          >
+                            <path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h6zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z" />
+                            <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
+                          </svg>
+                        </Box>
+                        <Box>Mobile Ticket Accepted</Box>
+                      </Flex>
+                    </Box>
+                  </Flex>
+                </Box>
               </Flex>
-              {/* })} */}
             </Box>
-
+            <br />
+            {/* ---------------------------------Dummy item2----------------------------------------- */}
+            {/* <Box>
+              <Flex>
+                <Box>
+                  <Flex direction="column" p={2}>
+                    <Box>
+                      <img
+                        src="https://media-cdn.tripadvisor.com/media/attractions-splice-spp-360x240/0a/b5/2f/ed.jpg"
+                        alt="image"
+                        width="80%"
+                      />
+                    </Box>
+                    <Box>
+                      <Flex gap={30}>
+                        <Box>Edit</Box>
+                        <Box>Remove</Box>
+                      </Flex>
+                    </Box>
+                  </Flex>
+                </Box>
+                <Box>
+                  <Flex direction="column" p={2}>
+                    <Box>
+                      <Heading size="md">
+                        <b>Pune Walking Tour</b>
+                      </Heading>
+                    </Box>
+                    <Box> Pune Walking tour - 9:00</Box>
+                    <Box> Pune Walking tour - 9:00</Box>
+                    <Box>
+                      <Flex gap={10}>
+                        <Box>
+                          2 Adults * <b>₹1743.36</b>
+                        </Box>
+                        <Box>
+                          <b>Total ₹3,486.72</b>
+                        </Box>
+                      </Flex>
+                      <Divider w="100%" color="gray" />
+                    </Box>
+                    <br />
+                    <Box>
+                      <Flex>
+                        <Box>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            fill="currentColor"
+                            class="bi bi-phone"
+                            viewBox="0 0 16 16"
+                          >
+                            <path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h6zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z" />
+                            <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
+                          </svg>
+                        </Box>
+                        <Box>Mobile Ticket Accepted</Box>
+                      </Flex>
+                    </Box>
+                  </Flex>
+                </Box>
+              </Flex>
+              <Divider w="100%" color="gray" />
+            </Box> */}
+            {/* })} */}
             {/* -----------------------Right side for total amount------------------------ */}
             <Box p={8} shadow="md" borderWidth="1px" className="review">
               <Heading as="h2" size="lg" padding="0.75rem">
@@ -84,12 +196,138 @@ const Cart = () => {
               <Button p={7} className="checkout-btn">
                 Checkout
               </Button>
-
-              <Container></Container>
+              <br />
+              <Container>
+                <Box padding="5% 0%" textAlign="left">
+                  <b>Reserve Now and Pay Later</b>
+                </Box>
+                <p className="unsure">
+                  Unsure of your plans? You can reserve a spot and pay for it
+                  later. Just click on "Checkout" to see more payment options.
+                  <b>Learn more</b>
+                </p>
+              </Container>
             </Box>
           </Flex>
-        </Container>
+        </Box>
       </Container>
+
+      <Box className="bottom">
+        <Box p={3}>
+          <Heading as="h2" size="lg" padding="0.75rem">
+            Book with Confidence
+          </Heading>
+        </Box>
+        <Divider w="100%" color="gray" />
+        <br />
+        <Box>
+          <Flex gap={5}>
+            <Box>
+              <Flex gap={2}>
+                <Box>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-wallet2"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M12.136.326A1.5 1.5 0 0 1 14 1.78V3h.5A1.5 1.5 0 0 1 16 4.5v9a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 13.5v-9a1.5 1.5 0 0 1 1.432-1.499L12.136.326zM5.562 3H13V1.78a.5.5 0 0 0-.621-.484L5.562 3zM1.5 4a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-13z" />
+                  </svg>
+                </Box>
+                <Box>
+                  <Flex direction="column">
+                    <Box>
+                      <b>Lowest Price Guarantee</b>
+                    </Box>
+                    <Box>Find it cheaper? We'll refund the difference</Box>
+                  </Flex>
+                </Box>
+              </Flex>
+            </Box>
+            <Box>
+              <Flex gap={2}>
+                <Box>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-file-lock"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M8 5a1 1 0 0 1 1 1v1H7V6a1 1 0 0 1 1-1zm2 2.076V6a2 2 0 1 0-4 0v1.076c-.54.166-1 .597-1 1.224v2.4c0 .816.781 1.3 1.5 1.3h3c.719 0 1.5-.484 1.5-1.3V8.3c0-.627-.46-1.058-1-1.224zM6.105 8.125A.637.637 0 0 1 6.5 8h3a.64.64 0 0 1 .395.125c.085.068.105.133.105.175v2.4c0 .042-.02.107-.105.175A.637.637 0 0 1 9.5 11h-3a.637.637 0 0 1-.395-.125C6.02 10.807 6 10.742 6 10.7V8.3c0-.042.02-.107.105-.175z" />
+                    <path d="M4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4zm0 1h8a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z" />
+                  </svg>
+                </Box>
+                <Box>
+                  <Flex direction="column">
+                    <Box>
+                      <b>24/7 Global Support</b>
+                    </Box>
+                    <Box>Get the answers you need, when you need them</Box>
+                  </Flex>
+                </Box>
+              </Flex>
+            </Box>
+            <Box>
+              <Flex gap={2}>
+                <Box>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-headphones"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M8 3a5 5 0 0 0-5 5v1h1a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V8a6 6 0 1 1 12 0v5a1 1 0 0 1-1 1h-1a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h1V8a5 5 0 0 0-5-5z" />
+                  </svg>
+                </Box>
+                <Box>
+                  <Flex direction="column">
+                    <Box>
+                      <b>Book Securely</b>
+                    </Box>
+                    <Box>We use SSL encryption to keep your data secure</Box>
+                  </Flex>
+                </Box>
+              </Flex>
+            </Box>
+            <Box>
+              <Flex gap={2}>
+                <Box>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-wallet2"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M12.136.326A1.5 1.5 0 0 1 14 1.78V3h.5A1.5 1.5 0 0 1 16 4.5v9a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 13.5v-9a1.5 1.5 0 0 1 1.432-1.499L12.136.326zM5.562 3H13V1.78a.5.5 0 0 0-.621-.484L5.562 3zM1.5 4a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-13z" />
+                  </svg>
+                </Box>
+                <Box>
+                  <Flex direction="column">
+                    <Box>
+                      <b>Call to Book</b>
+                    </Box>
+                    <Box>
+                      We're here to help and happy to assist you with your
+                      booking
+                    </Box>
+                    <Box>
+                      <u>Call now: +1 855 275 5071</u>
+                    </Box>
+                  </Flex>
+                </Box>
+              </Flex>
+            </Box>
+          </Flex>
+        </Box>
+      </Box>
     </>
   );
 };
