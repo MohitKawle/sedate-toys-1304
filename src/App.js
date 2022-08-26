7e79d990ed14a97cbde97be3328e91f05b6ed10e
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2022-08-26 23:52:01 +0530

    sss

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 
 import InnerNav from "./Components/InnerNavbar/InnerNav";
 import Homepage from "./Pages/Homepage";
-import MainRoutes from "./Pages/MainRoutes";
-import Slide from "./Components/Thingd_to_do/Slide";
 import Things from "./Components/Thingd_to_do/Things";
 import MainRoutes from "./Pages/MainRoutes";
 function App() {
