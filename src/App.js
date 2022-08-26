f97612fbb18d60ba0fded53326f568ba44e715e9
somyakant9 <msomya143@gmail.com>
2022-08-27 00:09:41 +0530

    Main Routes page added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-
-
-import Checkout_page from "./Components/Aditya/Checkout_page";
-import InnerNav from "./Components/InnerNavbar/InnerNav";
-import Homepage from "./Pages/Homepage";
-import Things from "./Components/Thingd_to_do/Things";
 import MainRoutes from "./Pages/MainRoutes";
+
 function App() {
   return (
     <div className="App">
-    <InnerNav/> 
-    <Things />
+     <MainRoutes/>
     </div>
 
   );
--- src/Pages/MainRoutes.jsx
+++ src/Pages/MainRoutes.jsx
@@ -4,22 +4,25 @@ import Homepage from './Homepage';
 import { Hotels } from '../Components/Hotels';
 import SinglePage from '../Components/Thingd_to_do/SinglePage';
 import Things from '../Components/Thingd_to_do/Things';
+import { Restaurants } from '../Components/Restaurants';
+import Basket from './Basket';
+import Checkout_page from '../Components/Aditya/Checkout_page';
+import Payment_page from '../Components/Aditya/Payment_page';
+
 const MainRoutes = () => {
   return (<>
     <Routes>
-    <Route path="/" element={<Homepage />} />
-
-    
-
-
-   <Route path="/hotels" element={<Hotels/>}/>
-   <Route path="/places/:id" element={<SinglePage />}/>
-   <Route path='/Things' element ={<Things />} />
- 
-
+      <Route path="/" element={<Homepage />} />
+      <Route path="/hotels" element={<Hotels />} />
+      <Route path="/restaurants" element={<Restaurants />} />
+      <Route path="/places/:id" element={<SinglePage />} />
+      <Route path='/things' element={<Things />} />
+      <Route path="/basket" element={<Basket />} />
+      <Route path="/checkout" element={<Checkout_page/>}/>
+      <Route path="/payment" element={<Payment_page/>}/>
     </Routes>
-   
-    </>)
+
+  </>)
 }
 
 export default MainRoutes
